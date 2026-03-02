0d527fbe24793be7386885e7f9caee685a11dc6f
Shinta-crab <scrab.engineer@gmail.com>
2026-03-02 20:58:40 +0900

    環境構築＋topページ作成＋render-build.sh追加

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+   # Deviseのコントローラーが動く時だけ、以下のメソッドを実行する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # 新規登録時に account_name を許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:account_name])
+    # アカウント編集時にも許可したい場合
+    devise_parameter_sanitizer.permit(:account_update, keys: [:account_name])
+  end
+end
