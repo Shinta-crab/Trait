fedebdee06fed2fb2a97b0dde6d7bb5097059f72
Shinta-crab <scrab.engineer@gmail.com>
2026-03-11 23:32:54 +0900

    lintエラー修正

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -6,10 +6,10 @@ class LikesController < ApplicationController
     @like = current_user.likes.find_or_initialize_by(photo_id: params[:photo_id])
     @like.touch if @like.persisted?
     if @like.save
-      render json: { status: 'liked' }
+      render json: { status: "liked" }
     else
       # 失敗時の処理がないと、保存に失敗したときにrenderが呼ばれずエラーになる可能性があります
-      render json: { status: 'error', errors: @like.errors.full_messages }, status: :unprocessable_entity
+      render json: { status: "error", errors: @like.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -18,6 +18,6 @@ class LikesController < ApplicationController
   def destroy_by_photo
     @like = current_user.likes.find_by(photo_id: params[:photo_id])
     @like.destroy if @like
-    render json: { status: 'unliked' }
+    render json: { status: "unliked" }
   end
 end
