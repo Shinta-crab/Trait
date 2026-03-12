34eacdd4fa5b597b725e3bd7b383545f96b81c32
Shinta-crab <scrab.engineer@gmail.com>
2026-03-12 11:48:46 +0900

    テストエラー解消

--- test/controllers/genres_controller_test.rb
+++ test/controllers/genres_controller_test.rb
@@ -1,6 +1,16 @@
 require "test_helper"
 
 class GenresControllerTest < ActionDispatch::IntegrationTest
+  # fixtures :all はテスト実行の最初に一度だけ行う
+  fixtures :all
+
+  setup do
+    # 依存関係を明示的にクリアしてからテストを開始する
+    @user = users(:one)
+    @genre = genres(:one)
+    @photo = photos(:one) # ここで photos が読み込まれることを保証
+    @like = likes(:one)
+  end
   test "should get index" do
     get genres_index_url
     assert_response :success
--- test/controllers/like_controller_test.rb
+++ test/controllers/like_controller_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class LikeControllerTest < ActionDispatch::IntegrationTest
+  fixtures :users, :analysis_results
   # test "the truth" do
   #   assert true
   # end
